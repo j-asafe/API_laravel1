07255f1f38453c10527741c729b30ac51a1c0230
DinoPimenteiro <gabrielwilliam234@gmail.com>
2025-08-22 22:37:37 -0300

    api inicio

---
+++ app/Http/Controllers/ProdutosController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produtos;
+use Illuminate\Http\Request;
+
+class ProdutosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Produtos $produtos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Produtos $produtos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Produtos $produtos)
+    {
+        //
+    }
+}
